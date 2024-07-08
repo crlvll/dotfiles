99352e2ca4e6accaa676126028d3f5a6860e4300
crlvll <rocinante@protonmail.com>
2024-07-08 21:21:52 +0200

    MarkdownPreview debug and reinstall

--- nvim/lua/cyril/plugins/markdown.lua
+++ nvim/lua/cyril/plugins/markdown.lua
@@ -1,36 +1,17 @@
 return {
-	-- add symbols-outline
-	{
-		-- "ixru/nvim-markdown",
-		-- cmd = "",
-		-- keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
+	-- install without yarn or npm
+	"iamcco/markdown-preview.nvim",
+	cmd = {
+		"MarkdownPreviewToggle",
+		"MarkdownPreview",
+		"MarkdownPreviewStop",
 	},
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
 
-	{
-		"iamcco/markdown-preview.nvim",
-		ft = "markdown",
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+	keys = {
+		{ "<leader>md", "<cmd>MarkdownPreview<cr>", desc = "Open Markdown Preview" },
 	},
-
-	-- {
-	--   "iamcco/markdown-preview.nvim",
-	--   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	--   ft = { "markdown" },
-	--   build = function()
-	--     vim.fn["mkdp#util#install"]()
-	--   end,
-	-- },
-
-	-- {
-	--   "lcheylus/overlength.nvim",
-	--   config = function()
-	--     require("overlength").setup({
-	--       textwidth_mode = 0,
-	--       default_overlength = 80,
-	--       disable_ft = { "qf", "help", "man", "checkhealth", "lazy", "packer", "NvimTree", "Telescope", "WhichKey" },
-	--     })
-	--   end,
-	-- },
 }
